0d00f436e458815056f80ea131ca3e5141d3e45e
abdallah abuzaid <abdallah.ayman1@msa.edu.eg>
2021-05-21 03:11:02 +0200

    added contact comp

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,15 @@
+import'./About.css'
 const About = () => {
     return (
-        <div>
-            <h1 style={{textAlign: "center"}}>About</h1>
-        </div>
+        
+            <div className="about-section">
+                <h1>About</h1>
+                <h2>We are MSA university students creating this computer science community web application</h2>
+                <h3>This web application is a community for all the computer science feild in Egypt it offers you Courses, Jobs vaccanies, Latest news of our feild and Blof for all programmers </h3>
+            </div>
+        
+        
+        
     )
 }
 
