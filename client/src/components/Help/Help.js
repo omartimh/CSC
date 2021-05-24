bb723cd76fa32ea445f6495dd68bfee162824d13
seifeltobgy <seif.ahmed1@msa.edu.eg>
2021-05-25 00:34:44 +0200

    search bar fy jobs w form help

--- client/src/components/Help/Help.js
+++ client/src/components/Help/Help.js
@@ -3,9 +3,35 @@ import React from 'react'
 
 const Help = () => {
     return (
-        <div className="help">
+    <div className="help">
             <h1 style={{textAlign: "center"}}>Help</h1>
+            
+            <div class="main-block">
+            <form action="/"/>
+        <div class="title">
+          <i class="fas fa-pencil-alt"></i> 
+          <h2>Ask here</h2>
         </div>
+        <div class="info">
+          <input class="fname" type="text" name="name" placeholder="Full name"/>
+          <input type="text" name="name" placeholder="Email"/>
+          <input type="text" name="name" placeholder="Phone number"/>
+          <input type="password" name="name" placeholder="Password"/>
+          <select>
+            <option value="type" selected>Problem type</option>
+            <option value="course">Course</option>
+            <option value="programming-lang">Proggraming language</option>
+            <option value="jobs">Jobs</option>
+       
+          </select>
+       </div>
+        <div class="checkbox">
+          <input type="checkbox" name="checkbox"/><span>I agree to the Privacy Poalicy <a href="">Privacy Poalicy </a></span>
+       </div>
+        <button type="submit" href="/">Submit</button>
+     </div>
+     
+   </div>
     )
 }
 
--- client/src/components/Jobs/Jobs.js
+++ client/src/components/Jobs/Jobs.js
@@ -1,10 +1,14 @@
 import './style.css';
 import React from 'react'
-
+import Input from '../Input/Input'
+import Button from '../Button/Button';
+import {ImSearch} from 'react-icons/im'
 const Jobs = () => {
     return (
         <div className="jobs">
             <h1 style={{textAlign: "center"}}>Jobs</h1>
+           <Input className="search-jobs" placeholder="Search for jobs" name="search" />
+           <Button className="btn-search" icon={<ImSearch/>}/>
         </div>
     )
 }
